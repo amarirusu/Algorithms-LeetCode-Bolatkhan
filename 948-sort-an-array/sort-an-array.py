c63b37b83820361db8000f8958b737102442a9ea
amaryllis <80963081+amarirusu@users.noreply.github.com>
2026-02-20 02:42:27 +0500

    Time: 1042 ms (50.48%) | Memory: 21.3 MB (59.64%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,31 +1,32 @@
-class Solution:
+class Solution(object):
     def sortArray(self, nums):
-        # Базовый случай: список из 1 элемента уже отсортирован
+        # Если в списке 1 элемент или меньше, он уже отсортирован
         if len(nums) <= 1:
             return nums
-
-        # Разделяем массив пополам
+        
+        # 1. Разделение: находим середину и делим массив
         mid = len(nums) // 2
-        left = self.sortArray(nums[:mid])
-        right = self.sortArray(nums[mid:])
-
-        # Сливаем две половины в одну отсортированную
-        return self.merge(left, right)
-
+        left_half = self.sortArray(nums[:mid])
+        right_half = self.sortArray(nums[mid:])
+        
+        # 2. Слияние: соединяем две части в одну
+        return self.merge(left_half, right_half)
+    
     def merge(self, left, right):
-        result = []
+        sorted_arr = []
         i = j = 0
         
-        # Сравниваем элементы из двух списков
+        # Сравниваем элементы из обеих половин
         while i < len(left) and j < len(right):
             if left[i] < right[j]:
-                result.append(left[i])
+                sorted_arr.append(left[i])
                 i += 1
             else:
-                result.append(right[j])
+                sorted_arr.append(right[j])
                 j += 1
         
-        # Добавляем остатки
-        result.extend(left[i:])
-        result.extend(right[j:])
-        return result
+        # Добавляем оставшиеся элементы (если они есть)
+        sorted_arr.extend(left[i:])
+        sorted_arr.extend(right[j:])
+        
+        return sorted_arr
